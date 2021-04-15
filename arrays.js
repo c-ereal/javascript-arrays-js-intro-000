11b0026224d0132d901e570287df66068eb43977
bere abreo <berenicej2013@gmail.com>
2021-04-15 05:49:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,17 +10,17 @@ function destructivelyAddElementToBeginningOfArray(array, name) {
   return(array);
 }
 
-function addElementToEndOfArray(array, element) {
-  [...array, "element"]
+function addElementToEndOfArray(array, name) {
+  return [...array, name]
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push("element");
+function destructivelyAddElementToEndOfArray(array, name) {
+  array.push(name);
   console.log(array);
 }
 
 function accessElementInArray(array, index) {
-  array[index];
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
@@ -30,6 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
   array.slice();
+  return array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
@@ -39,4 +40,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   array.slice(0, array.length - 1);
+  return array; 
 }
